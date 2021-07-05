881eee949c8887bf5e0d26f25c07bceb8aac2338
Madhusudhan R <m.r@axelor.com>
2021-07-05 16:14:27 +0530

    made use of JPA.em() instead of JpaSupport

--- modules/demo-expenseTracker/src/main/java/com/axelor/expense/service/PractiseRpcServiceImpl.java
+++ modules/demo-expenseTracker/src/main/java/com/axelor/expense/service/PractiseRpcServiceImpl.java
@@ -8,35 +8,29 @@ import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
-import com.axelor.db.JpaSupport;
+import com.axelor.db.JPA;
 
-public class PractiseRpcServiceImpl extends JpaSupport implements PractiseRpcService {
+public class PractiseRpcServiceImpl implements PractiseRpcService {
 
-	public List<Map<String,Object>> writeOneQuery(){
-		   System.out.println(
-			        "printing here for testing whether it is getting called or not");
-			    EntityManager em = getEntityManager();
-			    Query q1 = em.createQuery(" SELECT distinct" + 
-			    		"    _category.categoryName as category," + 
-			    		"     sum(self.price) as totalPriceOfAllExpense" + 
-			    		"    FROM" + 
-			    		"        Expense self" + 
-			    		"    inner join " + 
-			    		"    self.category as _category" + 
-			    		"    group by _category.categoryName");
-			    
-			    List<Object[]> l1 = q1.getResultList();
-			    List<Map<String, Object>> listdata = new ArrayList<Map<String, Object>>();
-			    for(Object[] item : l1) {
-			        Map<String, Object> data = new HashMap<>();
+	public List<Map<String, Object>> writeOneQuery() {
+		System.out.println("printing here for testing whether it is getting called or not");
+		EntityManager em = JPA.em();
+		Query q1 = em.createQuery(" SELECT distinct" + "    _category.categoryName as category,"
+				+ "     sum(self.price) as totalPriceOfAllExpense" + "    FROM" + "        Expense self"
+				+ "    inner join " + "    self.category as _category" + "    group by _category.categoryName");
 
-			    	String ncat = (String)item[0];
-			    	Long price = (Long)item[1];
-			    	data.put("ncat", ncat);
-			    	data.put("price", price);
-			    	listdata.add(data);
-			    	
-			    }
-			    return listdata;
+		List<Object[]> l1 = q1.getResultList();
+		List<Map<String, Object>> listdata = new ArrayList<Map<String, Object>>();
+		for (Object[] item : l1) {
+			Map<String, Object> data = new HashMap<>();
+
+			String ncat = (String) item[0];
+			Long price = (Long) item[1];
+			data.put("ncat", ncat);
+			data.put("price", price);
+			listdata.add(data);
+
+		}
+		return listdata;
 	}
 }
